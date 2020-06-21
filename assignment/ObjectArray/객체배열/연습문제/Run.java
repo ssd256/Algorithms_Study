c6e4c19948739962f1adbfc025709dd9af550e14
ssd256 <63248384+ssd256@users.noreply.github.com>
2020-06-21 12:55:12 +0900

    Create Run.java

---
+++ assignment/ObjectArray/객체배열/연습문제/Run.java
@@ -0,0 +1,12 @@
+import com.kh.practice.student.view.StudentMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		StudentMenu sm = new StudentMenu();
+		
+		
+	}
+	
+} // Run end
