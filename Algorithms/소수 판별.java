f4b1fb224617c1ca14423a2a1eefcc08ff6c312b
김미경 <63248384+ssd256@users.noreply.github.com>
2021-01-06 00:48:58 +0900

    Create 소수 판별.java

---
+++ Algorithms/소수 판별.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int num = Math.abs(Integer.parseInt(input));
+		
+		// 1과 자기 자신으로만 나누어져야 함
+		if(num%2 != 0 && num%3 != 0){
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+}
