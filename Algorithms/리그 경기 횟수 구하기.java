434d1c888d274f7ed277e2d0c2751ea85e22114d
김미경 <63248384+ssd256@users.noreply.github.com>
2021-01-06 20:53:11 +0900

    Create 리그 경기 횟수 구하기.java

---
+++ Algorithms/리그 경기 횟수 구하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		// 입력한 수 n에서 자기 자신을 뺀만큼 경기를 진행할 때 총 경기 수 구하기
+    // n=3 이면 1 + 2 = 3 
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int num = Integer.parseInt(input);
+    
+		int play = 0; 
+		for(int i = 0; i < num; i++){
+			play += i;
+		}
+		System.out.println(play);
+	}
+}
